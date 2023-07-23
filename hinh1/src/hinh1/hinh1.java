a6f5b7afcadbcb7300acdfae4332e24ef620d0f4
Pham Van Hinh <20102004hinh@gmail.com>
2023-07-23 21:45:36 +0700

    hinh

---
+++ hinh1/src/hinh1/hinh1.java
@@ -0,0 +1,11 @@
+package hinh1;
+
+public class hinh1 {
+	private int n;
+	public hinh1(int n) {
+		this.n = n;
+	}
+	public void hinh(){
+		System.out.println(this.n);
+	}
+}
---
+++ hinh1/src/hinh1/main.java
@@ -0,0 +1,8 @@
+package hinh1;
+
+public class main {
+	public static void main(String[] args) {
+		hinh1 h = new hinh1(1);
+		h.hinh();
+	}
+}
